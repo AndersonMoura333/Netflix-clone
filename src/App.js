60a1fb6180856d974466dfb858a83b29fc93c36c
Andersonmoura <andersonmouradasilva2@gmail.com>
2022-06-08 20:39:23 -0300

    navBar add

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import categories from './api';
 import Row from './components/Row/index'
 import Banner from './components/Banner/Banner';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Banner/>
      {categories.map((category)=> {
        return <Row 
